afbea35716969a66a49757e187eac604a03290d1
shuheng0330 <thongshuheng0330@gmail.com>
2023-12-12 22:01:33 +0800

    Second commitVivaMethod.java

--- VivaMethod.java
+++ VivaMethod.java
@@ -14,7 +14,7 @@ public class VivaMethod {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
         // TODO code application logic here
     }
     
