e4b3d1303f76707b8b0a5693f7a19f91b95b4832
shuheng0330 <thongshuheng0330@gmail.com>
2023-12-13 18:42:26 +0800

    first commit

---
+++ Q1Exttra.java
@@ -0,0 +1,33 @@
+package viva2;
+import java.util.Scanner;
+public class Q1Exttra {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a password:");
+        String password=input.next();
+        boolean validpassword=IsPasswordValid(password); //method call
+        System.out.println(validpassword);
+    }
+    public static boolean IsPasswordValid(String p){
+        int lower=0;
+        int upper=0;
+        int digit=0;
+        int special=0;
+        for (int i=0;i<p.length();i++){
+            char a=p.charAt(i);
+            if (Character.isUpperCase(a))
+                upper++;
+            if (Character.isLowerCase(a))
+                lower++;
+            if (Character.isDigit(a))
+                digit++;
+            if (a=='!'||a=='@'||a=='#'||a=='$'||a=='%'||a=='^'||a=='&'||a=='*'||a=='('||a==')'||a=='-'||a=='+')
+                special++;  
+        }
+        if (p.length()>=8 && upper>=1 && lower>=1 && digit>=3 && special==1)
+            return true;
+        
+        return false;
+}}
+    
+
